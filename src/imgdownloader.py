a5b7a63b277c8a4a84e9d58421ec095cdd53c469
Toru Aoike <t-aoike@ndl.go.jp>
2025-02-10 14:14:30 +0900

    #実行時の問題 #4指摘に基づく修正

--- src/imgdownloader.py
+++ src/imgdownloader.py
@@ -1,9 +1,7 @@
 import requests
-import json
 import os
 import pandas as pd
 import time
-import shutil
 import sys
 import glob
 
@@ -14,7 +12,7 @@ if __name__ == '__main__':
     if len(args)>=2:
         if args[1]=="v1":
            mode="v1"
-        elif args[2]=="v2":
+        elif args[1]=="v2":
             mode="v2"
         else:
             print("The parameter must be 'v1 'or 'v2'.")
@@ -25,7 +23,7 @@ if __name__ == '__main__':
         #sys.exit()
 
     if mode=="v1":
-        metadf=pd.read_csv("v1_metadata.csv",dtype=str)
+        metadf=pd.read_csv("v1_metadata.csv",sep="\t",dtype=str)
         metadf=metadf[~metadf["Image URL"].isna()]
         keylist=[m1+"_"+m2 for m1,m2 in zip(metadf["Book ID"],metadf["File ID(NDL)"])]
         key2imgurl = dict(zip(keylist, metadf["Image URL"]))
@@ -37,13 +35,16 @@ if __name__ == '__main__':
                 key=bookid+"_"+fileid
                 if key in key2imgurl:
                     imgurl=key2imgurl[key]
+                    #過去取得済みの画像をスキップする
+                    if os.path.exists(os.path.join("img_v1", bookid, fileid+".jpg")):
+                        continue
                     rawimg = requests.get(imgurl).content
                     os.makedirs(os.path.join("img_v1", bookid), exist_ok=True)
                     with open(os.path.join("img_v1", bookid, fileid+".jpg"), "wb") as fout:
                         fout.write(rawimg)
                         time.sleep(1)
     if mode=="v2":
-        metadf=pd.read_csv("v2_metadata.csv",dtype=str)
+        metadf=pd.read_csv("v2_metadata.csv",sep="\t",dtype=str)
         metadf=metadf[~metadf["Image URL"].isna()]
         keylist=[m1+"_"+m2+"_"+m3 for m1,m2,m3 in zip(metadf["Project ID"],metadf["Book ID"],metadf["File ID(Minna De Honkoku)"])]
         key2imgurl = dict(zip(keylist, metadf["Image URL"]))
@@ -56,6 +57,9 @@ if __name__ == '__main__':
                     key=projectid+"_"+bookid+"_"+fileid
                     if key in key2imgurl:
                         imgurl=key2imgurl[key]
+                        #過去取得済みの画像をスキップする
+                        if os.path.exists(os.path.join("img_v2",projectid, bookid, fileid+".jpg")):
+                            continue
                         rawimg = requests.get(imgurl).content
                         os.makedirs(os.path.join("img_v2", projectid,bookid), exist_ok=True)
                         with open(os.path.join("img_v2",projectid, bookid, fileid+".jpg"), "wb") as fout:
